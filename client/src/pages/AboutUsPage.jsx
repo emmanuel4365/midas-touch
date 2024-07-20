2557a3dd8f154661bf71d08b489bc4baa3cf353a
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-07-20 23:27:59 +0100

    added content to about page

--- client/src/pages/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage.jsx
@@ -1,7 +1,29 @@
 const AboutUsPage = () => {
   return (
     <main className="about-page">
-      <div className="about-page-center">AboutUsPage</div>
+      <div className="about-page-center">
+        <h1 className="about-us-heading">About Us</h1>
+        <hr width="100px" color="#a9ce4d" />
+        <p className="about-us-paragraph p1">
+          At Midas Touch Consultancy, we are a distinguished firm of
+          professional Accountants, Tax Practitioners, and Management Experts.
+          Our team is equipped with topnotch academic qualifications, diverse
+          industry experience and a profound acumen, dedicated to empowering
+          organizations through capacity building and strategic guidance. We
+          take pride in enhancing the performance of{" "}
+          <strong className="ican">ICAN</strong> and{" "}
+          <strong className="aatswa">AATSWA</strong> students, offering
+          unparalleled teaching skills that foster academic excellence.
+        </p>
+        <p className="about-us-paragraph p2">
+          Additionally, we provide comprehensive{" "}
+          <strong className="jamb">JAMB</strong> tutorial services, utilizing
+          both virtual and onsite methods to ensure every candidate achieves
+          their highest potential. At Midas Touch Consultancy, we are committed
+          to your success as an organization or individual. We take pride in
+          your growth and success!
+        </p>
+      </div>
     </main>
   );
 };
