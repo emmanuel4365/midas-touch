cbd7a66f13d7aae60366e0c3babcaeb6046a9820
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-07-22 23:18:04 +0100

    adding content to learning and dev page started

--- client/src/pages/LearningAndDevelopmentPage.jsx
+++ client/src/pages/LearningAndDevelopmentPage.jsx
@@ -2,7 +2,8 @@ const LearningAndDevelopmentPage = () => {
   return (
     <main className="learning-and-development-page">
       <div className="learning-and-development-page-center">
-        LearningAndDevelopmentPage
+        <h1 className="about-us-heading">Learning and Development</h1>
+        <hr width="100px" color="#a9ce4d" />
       </div>
     </main>
   );
