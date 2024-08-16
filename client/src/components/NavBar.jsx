d0b919232d112d7807e5a3f85747e4168c422437
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-08-16 11:28:01 +0100

    created top-address-bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,36 @@
 import { Link } from "react-router-dom";
 import LinksBar from "./LinksBar";
 import midasLogo from "../assets/midas-touch-logo.png";
-import SearchBar from "./SearchBar";
 import { MdFormatAlignJustify } from "react-icons/md";
+import addressIcon from "../assets/address-icon.svg";
+import emailIcon from "../assets/email-icon.svg";
+// import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
+      <div className="top-address-bar">
+        <div className="top-address-center">
+          <div className="top-address-contact email">
+            <img src={emailIcon} alt="" />
+            <span>midastouchconsult33@gmail.com</span>
+          </div>
+          <div className="top-address-contact address">
+            <img src={addressIcon} alt="" />
+            <span>23 Kunle Ogungba Street, Lekki, Lagos</span>
+          </div>
+        </div>
+      </div>
       <div className="logo-links-center">
         <div className="midas-logo-container">
           <Link to="/">
             <img src={midasLogo} alt="midas logo" className="midas-logo" />
           </Link>
         </div>
-        <SearchBar />
+        {/* <SearchBar /> */}
         <LinksBar />
         <MdFormatAlignJustify className="justify-icon" cursor="pointer" />
-        {/* <div className="search">Search</div> */}
-        {/* <CartAndUser /> */}
       </div>
-      {/* <ProductsLinksBar /> */}
     </nav>
   );
 };
