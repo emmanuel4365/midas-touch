b67792e5bd310483e66b6b3c4a7d6e09841bf521
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-08-21 18:18:03 +0100

    finished corporate page

--- client/src/pages/CorporateAndAdvisoryPage.jsx
+++ client/src/pages/CorporateAndAdvisoryPage.jsx
@@ -5,11 +5,46 @@ const CorporateAndAdvisoryPage = () => {
         <h1 className="corporate-heading">Corporate Services and Advisories</h1>
         <hr width="100px" color="#a9ce4d" />
         <ul className="corporate-items-container">
-          <li>Internal Control and Compliance</li>
-          <li>CashFlow Audit</li>
-          <li>Sales Audit</li>
-          <li>Emotional Intelligence</li>
-          <li>Performance Management</li>
+          <li>
+            <strong>Tax Computation, Filing and Advisory services:</strong> We
+            assist organizations in accurately calculating and remitting their
+            tax liabilities to the appropriate tax authorities. Additionally, we
+            offer comprehensive tax consultancy services, providing expert
+            advice and guidance to support organizations in navigating complex
+            tax matters.
+          </li>
+          <li>
+            <strong>Financial Report Preparation and Analysis:</strong> We help
+            organizations to prepare their financial reports (Income Statement,
+            CashFlow Statements and Statement of Financial Position or
+            Balance-Sheet) and provide professional insight on organization’s
+            performance for the periods.
+          </li>
+          <li>
+            <strong>Internal Control and Financial System:</strong> we
+            facilitate the set-up of a full internal control mechanism for
+            organizations, curbing theft, pilferages and safe-guard assets and
+            resources of the organization. In addition, we specialize in setting
+            up and properly restructuring the account and or finance department
+            of organizations.
+          </li>
+          <li>
+            <strong>Inventory (Stock Count) Count:</strong> Our team of expert
+            facilitate the monthly, quarterly or annual inventory count of
+            organizations, providing independent verification as to cost and
+            quantity of stock. Do you have large volume of inventory? We can
+            help in professionally organizing, counting and verifying them for
+            your decision making.
+          </li>
+          <li>
+            <strong>Financial & Sales Audit:</strong> we provide independent
+            assessment of organization's financial statements and Sales reports
+            ensuring that the reports are accurate and in compliance with
+            generally accepted accounting principles (GAAP), or International
+            Financial Reporting Standards (IFRS), donor regulations and
+            organizational policies; preparing them therefore, for external and
+            donor audits in the case of NGOs.
+          </li>
           <li>Customer Service Consultancy</li>
         </ul>
       </div>
