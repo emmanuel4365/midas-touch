96159617893f3d6da83855d8397a0893ca1ca248
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-08-21 18:09:29 +0100

    finished learning and dev page

--- client/src/pages/LearningAndDevelopmentPage.jsx
+++ client/src/pages/LearningAndDevelopmentPage.jsx
@@ -4,13 +4,81 @@ const LearningAndDevelopmentPage = () => {
       <div className="learning-and-development-page-center">
         <h1 className="learning-heading">Learning and Development</h1>
         <hr width="100px" color="#a9ce4d" />
+        <p>
+          The learning and development arm of the firm encompasses the following
+          broad areas:
+        </p>
         <ul className="learning-items-container">
-          <li>ICAN Tutorial Services</li>
-          <li>ACCA Tutorial Services</li>
-          <li>ICSAN Tutorial Services</li>
-          <li>CITN Tutorial Servicess</li>
-          <li>AATSWA Tutorial Services</li>
-          <li>JAMB Tutorial Services</li>
+          <li>
+            <strong>ICAN Exam preparation training:</strong> Our team of
+            professional tutors delivers ICAN training to individuals aspiring
+            to become Chartered Accountants. Our proven methodology has resulted
+            in an impressive 80% pass rate per diet. We offer lectures both
+            virtually and on-site, ensuring a serene and conducive learning
+            environment.
+          </li>
+          <li>
+            <strong>ICAN ATS Training:</strong> The institute's ATS examination
+            is conducted twice a year, in March and September. We enhance
+            students' success rates by offering personalized classroom
+            instruction and comprehensive guidance. Prospective students with
+            five credits including English, Mathematics, and Economics in WAEC
+            or NECO are eligible to enroll in this program, which provides the
+            fastest pathway to becoming a Chartered Accountant, even before
+            university graduation.
+          </li>
+          <li>
+            <strong>JAMB Lesson:</strong> Our experienced tutors offer
+            comprehensive JAMB lessons designed to help students achieve top
+            scores. We provide tailored instruction across all subject areas,
+            utilizing proven teaching methods to ensure thorough preparation for
+            the exam. Lessons are available both virtually and on-site, in a
+            supportive and focused learning environment.
+          </li>
+          <li>
+            <strong>Emotional Intelligence Training:</strong> This program is
+            designed to help individuals and teams enhance their self-awareness,
+            empathy, and interpersonal skills. Through practical exercises and
+            expert guidance, participants will learn how to manage their
+            emotions effectively, improve communication, and foster better
+            relationships both personally and professionally. Training is
+            available both in-person and online, tailored to meet the specific
+            needs of your organization.
+          </li>
+          <li>
+            <strong>Leadership Training:</strong> Our Leadership Training
+            program is designed to equip individuals with the skills and
+            strategies needed to lead effectively in today’s dynamic
+            environment. Through interactive sessions and real-world scenarios,
+            participants will develop key leadership competencies, including
+            decision-making, team management, and strategic thinking. The
+            training is available both on-site and online, customized to meet
+            the specific leadership development needs of your organization.
+          </li>
+          <li>
+            <strong>Data Analysis Training:</strong> This course is tailored to
+            equip participants with the essential skills to gather, analyze,
+            interpret, and present data effectively for business decision
+            making. Through hands-on experience and expert instruction,
+            participants will learn how to use data analysis tools, apply
+            statistical methods, and draw actionable insights to support
+            decision-making. This training is available both online and on-site,
+            with content customized to suit the specific needs of your
+            organization.
+          </li>
+          <li>
+            <strong>Public Speaking and Presentation Training:</strong> Public
+            speaking and presentation skills are critical for effective
+            communication, career growth, confidence, influence, and personal
+            development. They are fundamental for anyone looking to lead,
+            inspire, and succeed in both professional and personal spheres. This
+            course is designed to help individuals build confidence and master
+            the art of effective communication. Participants will learn
+            techniques for engaging an audience, structuring compelling
+            presentations, and delivering messages with clarity and impact.
+            Through practical exercises and personalized feedback, this training
+            prepares individuals to speak with confidence in any setting.
+          </li>
         </ul>
       </div>
     </main>
