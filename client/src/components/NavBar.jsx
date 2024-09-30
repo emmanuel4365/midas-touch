60be046d33f89d2184544242de7cf40617fa4650
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-09-30 12:49:34 +0100

    adjusted to mobile view

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -13,13 +13,11 @@ const NavBar = () => {
         <div className="top-address-center">
           <div className="top-address-contact email">
             <img src={emailIcon} alt="" width="16px" height="16px" />
-            <span style={{ fontSize: "12px" }}>
-              midastouchconsult33@gmail.com
-            </span>
+            <span>midastouchconsult33@gmail.com</span>
           </div>
           <div className="top-address-contact address">
             <img src={addressIcon} alt="" width="16px" height="16px" />
-            <span style={{ fontSize: "12px" }}>+234 916 666 1459</span>
+            <span>+234 916 666 1459</span>
           </div>
         </div>
       </div>
